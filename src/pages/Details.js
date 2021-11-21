cdeaee8ba854cf944f6ae24d3ace43a3cbdf5cbd
Kain724 <tiberius21ad@gmail.com>
2021-11-21 13:24:50 +0200

    details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,5 +1,30 @@
-const Details = ({match}) => {
-  return <div>Details {match.params.name}</div>
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { IoArrowBack } from 'react-icons/io5'
+import { useHistory, useParams } from 'react-router'
+import { searchByCountry } from '../API'
+import { Info } from '../components/Info'
+
+import MyButton from '../components/MyButton'
+
+const Details = () => {
+  const { name } = useParams()
+  const { push, goBack } = useHistory()
+  const [country, setCountry] = useState(null)
+
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]))
+  }, [name])
+
+  return (
+    <div>
+      <MyButton onClick={goBack}>
+        <IoArrowBack /> Back
+      </MyButton>
+      {country &&  <Info push={push} {...country} />}
+    </div>
+  )
 }
 
 export default Details
